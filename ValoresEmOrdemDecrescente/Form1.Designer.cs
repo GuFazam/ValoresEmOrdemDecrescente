682f8db77c22e3003648038ff2fcb5b5dcedc4b7
Gustavo H. Fazam <112525498+GuFazam@users.noreply.github.com>
2023-04-19 21:23:30 -0300

    Exercícios: Ordem decrescente de 3 valores, qual valor é maior e qual é menor e tabuada feito

--- ValoresEmOrdemDecrescente/Form1.Designer.cs
+++ ValoresEmOrdemDecrescente/Form1.Designer.cs
@@ -33,6 +33,13 @@
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.btnOrdenar = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
@@ -75,11 +82,77 @@
             this.btnOrdenar.UseVisualStyleBackColor = true;
             this.btnOrdenar.Click += new System.EventHandler(this.btnOrdenar_Click);
             // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(349, 52);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(349, 94);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 6;
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(362, 132);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(75, 23);
+            this.btnVerificar.TabIndex = 7;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(362, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 8;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(630, 52);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 9;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(644, 91);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 10;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(654, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 11;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(324, 376);
+            this.ClientSize = new System.Drawing.Size(922, 376);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.btnOrdenar);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox3);
@@ -99,6 +172,13 @@
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnOrdenar;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- ValoresEmOrdemDecrescente/Form1.cs
+++ ValoresEmOrdemDecrescente/Form1.cs
@@ -32,5 +32,47 @@ namespace ValoresEmOrdemDecrescente
             // Exibe os valores em ordem decrescente no Label
             label1.Text = numeros[0] + ", " + numeros[1] + ", " + numeros[2];
         }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            // Lê os valores digitados pelo usuário nas TextBoxes
+            int num1 = int.Parse(textBox4.Text);
+            int num2 = int.Parse(textBox5.Text);
+
+            if (num1 > num2)
+            {
+                // num1 é maior que num2
+                label2.Text = num1 + " é maior que: " + num2;
+            }
+            else if (num1 < num2)
+            {
+                // num1 é menor que num2
+                label2.Text = num1 + " é menor que: " + num2;
+            }
+            else if (num1 == num2)
+            {
+                // num1 e num2 são iguais
+                label2.Text = num1 + " é igual à " + num2;
+            }
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            // Lê o número digitado pelo usuário na TextBox
+            int num = int.Parse(textBox6.Text);
+
+            // Cria uma variável para armazenar a tabuada do número
+            string tabuada = "";
+
+            // Faz um loop de 1 a 10 para calcular a tabuada do número
+            for (int i = 1; i <= 10; i++)
+            {
+                int resultado = num * i;
+                tabuada += num + " x " + i + " = " + resultado + "\n";
+            }
+
+            // Exibe a tabuada do número no Label
+            label3.Text = "Tabuada do " + num + ":\n\n" + tabuada;
+        }
     }
 }
